cea87c294e8011687cd66266be8be7f59bf449fc
Tyler Hardy <35210905+thardy2001@users.noreply.github.com>
2018-03-19 19:35:03 +1100

    Rook movement, bishop movement, queen movement, king movement all defined and working (sort of)

--- gameFunctions.py
+++ gameFunctions.py
@@ -163,10 +163,18 @@ def checkMoveKnight(start_rank, start_column, end_rank, end_column, team,start_p
     return True
 
 def checkMoveBishop(start_rank, start_column, end_rank, end_column, team,start_pos, end_pos, board):
-    return True
+    if start_pos != end_pos:
+        if abs(changeRankToDigit(start_rank) - changeRankToDigit(end_rank)) == abs(start_column - end_column):
+            if checkTeam(end_pos, board) != team:
+                return True
+    return False
 
 def checkMoveKing(start_rank, start_column, end_rank, end_column, team,start_pos, end_pos, board):
-    return True
+    if start_pos != end_pos:
+        if abs(changeRankToDigit(start_rank) - changeRankToDigit(end_rank)) == 1 or abs(start_column - end_column) == 1 or abs(changeRankToDigit(start_rank) - changeRankToDigit(end_rank)) == 1 and abs(start_column - end_column) == 1 :
+            if checkTeam(end_pos, board) != team:
+                return True
+    return False
 
 def checkMoveQueen(start_rank, start_column, end_rank, end_column, team,start_pos, end_pos, board):
     if checkMoveRook(start_rank, start_column, end_rank, end_column, team,start_pos, end_pos, board):
